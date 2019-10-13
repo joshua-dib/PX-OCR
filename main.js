c471ce555afcc4785bedfd072c40847df967ecc8
Joshua Dib <18993745@student.westernsydney.edu.au>
2019-10-13 15:27:05 +1100

    NO TABS - most other design change

--- main.js
+++ main.js
@@ -20,9 +20,10 @@ function createWindow () {
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
+  mainWindow.maximize(true)
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  //mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
